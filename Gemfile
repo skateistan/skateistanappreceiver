7016787f30756459878f0e2bd06c49cf2a83d273
James Dennes <jdennes@gmail.com>
2015-01-21 19:00:23 +0100

    Use double quotes consistently everywhere

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'rake', '~> 10.0'
-gem 'sinatra', '~> 1.4'
-gem 'highrise', '~> 3.0'
-gem 'createsend', '~> 3.1'
-gem 'omniauth-createsend', '~> 1.0'
-gem 'newrelic_rpm', '~> 3.6'
-gem 'heroku-api', '~> 0.3'
+gem "rake", "~> 10.0"
+gem "sinatra", "~> 1.4"
+gem "highrise", '~> 3.0'
+gem "createsend", "~> 3.1"
+gem "omniauth-createsend", "~> 1.0"
+gem "newrelic_rpm", "~> 3.6"
+gem "heroku-api", "~> 0.3"
 
 ruby "2.0.0"
--- app.rb
+++ app.rb
@@ -1,30 +1,30 @@
-require 'bundler/setup'
-require 'sinatra' unless defined?(Sinatra)
-require 'highrise'
-require 'createsend'
-require 'omniauth-createsend'
-require 'heroku-api'
+require "bundler/setup"
+require "sinatra" unless defined?(Sinatra)
+require "highrise"
+require "createsend"
+require "omniauth-createsend"
+require "heroku-api"
 
 configure do
-  require 'newrelic_rpm' if production?
+  require "newrelic_rpm" if production?
 
   Highrise::Base.format = :xml
-  Highrise::Base.site = ENV['HIGHRISE_URL']
-  Highrise::Base.user = ENV['HIGHRISE_API_TOKEN']
+  Highrise::Base.site = ENV["HIGHRISE_URL"]
+  Highrise::Base.user = ENV["HIGHRISE_API_TOKEN"]
 end
 
 use Rack::Session::Cookie
 use OmniAuth::Builder do
-  provider :createsend, ENV['CAMPAIGN_MONITOR_CLIENT_ID'],
-    ENV['CAMPAIGN_MONITOR_CLIENT_SECRET'],
-    :scope => 'ManageLists,ImportSubscribers'
+  provider :createsend, ENV["CAMPAIGN_MONITOR_CLIENT_ID"],
+    ENV["CAMPAIGN_MONITOR_CLIENT_SECRET"],
+    :scope => "ManageLists,ImportSubscribers"
 end
 
 # The user goes to /auth/createsend to initiate the OAuth exchange.
 # After the user is authenticated, they are redirected here.
-get '/auth/:provider/callback' do
-  access_token = request.env['omniauth.auth']['credentials']['token']
-  refresh_token = request.env['omniauth.auth']['credentials']['refresh_token']
+get "/auth/:provider/callback" do
+  access_token = request.env["omniauth.auth"]["credentials"]["token"]
+  refresh_token = request.env["omniauth.auth"]["credentials"]["refresh_token"]
 
   response = "<pre>"
   response << "You're authenticated - here's what you need:<br/><br/>"
@@ -34,32 +34,32 @@ get '/auth/:provider/callback' do
   response
 end
 
-get '/' do
+get "/" do
   "Skateistan online application receiver."
 end
 
 def add_cm_subscriber(email, name, custom_fields)
   auth = {
-    :access_token => ENV['CAMPAIGN_MONITOR_ACCESS_TOKEN'],
-    :refresh_token => ENV['CAMPAIGN_MONITOR_REFRESH_TOKEN']
+    :access_token => ENV["CAMPAIGN_MONITOR_ACCESS_TOKEN"],
+    :refresh_token => ENV["CAMPAIGN_MONITOR_REFRESH_TOKEN"]
   }
   begin
     tries ||= 2
     CreateSend::Subscriber.add(
-      auth, ENV['CAMPAIGN_MONITOR_LIST_ID'], email, name, custom_fields, true)
+      auth, ENV["CAMPAIGN_MONITOR_LIST_ID"], email, name, custom_fields, true)
     rescue CreateSend::ExpiredOAuthToken => eot
       access_token, expires_in, refresh_token =
         CreateSend::CreateSend.refresh_access_token auth[:refresh_token]
-      # Doing `ENV['CAMPAIGN_MONITOR_ACCESS_TOKEN'] = access_token` here
+      # Doing `ENV["CAMPAIGN_MONITOR_ACCESS_TOKEN"] = access_token` here
       # would not persist the environment variable, which would mean that
       # we would end up refreshing the access token for every single request.
       # So instead, we use the Heroku API to set CAMPAIGN_MONITOR_ACCESS_TOKEN.
       # Persisting CAMPAIGN_MONITOR_ACCESS_TOKEN means that we only need to
       # refresh tokens when the current access token has expired.
-      heroku = Heroku::API.new # Assumes ENV['HEROKU_API_KEY'] is set
+      heroku = Heroku::API.new # Assumes ENV["HEROKU_API_KEY"] is set
       heroku.put_config_vars(
-        'skateistanappreceiver',
-        'CAMPAIGN_MONITOR_ACCESS_TOKEN' => access_token)
+        "skateistanappreceiver",
+        "CAMPAIGN_MONITOR_ACCESS_TOKEN" => access_token)
       auth[:access_token] = access_token
       retry unless (tries -= 1).zero?
       p "Error: #{eot}"
@@ -67,16 +67,16 @@ def add_cm_subscriber(email, name, custom_fields)
 end
 
 # Receive intern application
-post '/a/?' do
-  name = params['name']
-  email = params['email']
-  note = params['note']
-  skills = params['skills']
+post "/a/?" do
+  name = params["name"]
+  email = params["email"]
+  note = params["note"]
+  skills = params["skills"]
 
   person = Highrise::Person.create :name => name,
     :contact_data => {
       :email_addresses => [
-        { :address => email, :location => 'Home' }
+        { :address => email, :location => "Home" }
       ]
     },
     :subject_datas => [
@@ -85,7 +85,7 @@ post '/a/?' do
   person.tag! "intern applicant"
   person.add_note :body => note
 
-  custom_fields = [{ :Key => 'type', :Value => 'ia' }]
+  custom_fields = [{ :Key => "type", :Value => "ia" }]
   add_cm_subscriber email, name, custom_fields
 
   # Respond with 201 Created, and set the body as the applicant's Highrise URL
@@ -94,21 +94,21 @@ post '/a/?' do
 end
 
 # Receive remote volunteer application
-post '/rva/?' do
-  name = params['name']
-  email = params['email']
-  note = params['note']
+post "/rva/?" do
+  name = params["name"]
+  email = params["email"]
+  note = params["note"]
 
   person = Highrise::Person.create :name => name,
     :contact_data => {
       :email_addresses => [
-        { :address => email, :location => 'Home' }
+        { :address => email, :location => "Home" }
       ]
     }
   person.tag! "remote applicant"
   person.add_note :body => note
 
-  custom_fields = [{ :Key => 'type', :Value => 'rva' }]
+  custom_fields = [{ :Key => "type", :Value => "rva" }]
   add_cm_subscriber email, name, custom_fields
 
   # Respond with 201 Created, and set the body as the applicant's Highrise URL
